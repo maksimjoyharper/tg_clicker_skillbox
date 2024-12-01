69e2cd98212ead27021552a8498ed9d10c108845
solovey.e.v <e.v.solovey@inbox.ru>
2024-12-02 02:11:50 +0300

    fix

--- telegram.py
+++ telegram.py
@@ -44,31 +44,3 @@ async def main() -> None:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
-
-
-@dp.message(CommandStart())
-async def handle_start(message: types.Message):
-    command_parts = message.text.split()
-    if len(command_parts) > 1:
-        referrer_id = command_parts[1].replace('id_', '')  # Удаляем префикс 'id_'
-        web_app_url = f'https://eggswar.com?id={referrer_id}'
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="Запуск", web_app=WebAppInfo(url=web_app_url))]
-        ])
-        await message.answer("Начни сейчас и догони своего друга", reply_markup=keyboard)
-    else:
-        web_app_url = 'https://eggswar.com/'
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="Запуск", web_app=WebAppInfo(url=web_app_url))]
-        ])
-        await message.answer("Попробуй свои силы и одолей всех врагов у себя на пути", reply_markup=keyboard)
-
-
-async def main() -> None:
-    # And the run events dispatching
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
